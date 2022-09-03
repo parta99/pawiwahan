64c758476fbf67e662b9673a010404d017d9a55a
parta99 <dejon933@gmail.com>
2022-09-03 23:15:20 +0800

    better than change same code

--- assets/js/pawiwahan.js
+++ assets/js/pawiwahan.js
@@ -78,3 +78,20 @@ function copyToClipboard(element) {
 function toggleAudio(value) {
   return !value.checked ? myAudio.play() : myAudio.pause();
 }
+// scroll to-top
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
