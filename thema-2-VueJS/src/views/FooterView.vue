1b693e55c798adcecf23ca9000849dd34cb12e5d
DE Juniada <dejon933@gmail.com>
2024-04-19 10:48:50 +0800

    Update FooterView.vue

--- thema-2-VueJS/src/views/FooterView.vue
+++ thema-2-VueJS/src/views/FooterView.vue
@@ -1,12 +1,24 @@
 <script setup>
- import SocialItem from '@/components/SocialItem.vue';
+import SocialItem from '@/components/SocialItem.vue';
+import { ref , onMounted } from 'vue';
+
+const currentYear  = ref('');
+
+onMounted(() => {
+  getCurrentYear();
+});
+
+function getCurrentYear(){
+  const year = new Date().getFullYear();
+  currentYear.value = year.toString();
+}
 </script>
 <template>
   <section class="card">
     <div class="container">
       <div class="col">
         <div class="item-text">
-        <h2> Wedding Invitation &copy; 2024 By <a href="https://github.com/parta99" target="_blank" rel="noopener noreferrer">DE Juna</a></h2>
+        <h2> Wedding Invitation &copy; 2022 - {{ currentYear }} By <a href="https://github.com/parta99" target="_blank" rel="noopener noreferrer">DE Juna</a></h2>
         <SocialItem></SocialItem>
       </div>
       </div>
@@ -22,5 +34,8 @@
   }
   a{
     cursor: pointer;
+    text-decoration: none;
+    color: #5639af;
+    font-weight: 700;
   }
 </style>
